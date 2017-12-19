e40316584cc1243852f91cc8c5b54beb8510ae3a
Chad Smith <cs01@users.noreply.github.com>
2017-12-18 19:00:16 -0800

    move StringStream to file; add time complexity test to unit test

---
+++ pygdbmi/StringStream.py
@@ -0,0 +1,86 @@
+from pygdbmi.printcolor import print_cyan
+
+
+class StringStream:
+    """A simple class to hold text so that when passed
+    between functions, the object is passed by reference
+    and memory does not need to be repeatedly allocated for the string.
+
+    This class was written here to avoid adding a dependency
+    to the project rather than using.
+    """
+
+    def __init__(self, raw_text, debug=False):
+        self.raw_text = raw_text
+        self.index = 0
+        self.len = len(raw_text)
+        self.debug = debug
+
+    def read(self, count):
+        """Read count characters starting at self.index,
+        and return those characters as a string
+        """
+        new_index = self.index + count
+        if new_index > self.len:
+            buf = self.raw_text[self.index:]  # return to the end, don't fail
+        else:
+            buf = self.raw_text[self.index:new_index]
+        self.index = new_index
+
+        return buf
+
+    def seek(self, offset):
+        """Advance the index of this StringStream by offset characters"""
+        self.index = self.index + offset
+
+    def advance_past_chars(self, chars):
+        """Advance the index past specific chars
+        Args chars (list): list of characters to advance past
+
+        Return substring that was advanced past
+        """
+        start_index = self.index
+        while True:
+            current_char = self.raw_text[self.index]
+            self.index += 1
+            if current_char in chars:
+                break
+            elif self.index == self.len:
+                break
+        return self.raw_text[start_index:self.index - 1]
+
+    def advance_past_string_with_gdb_escapes(self, chars_to_remove_gdb_escape=['"']):
+        """characters that gdb escapes that should not be
+        escaped by this parser
+        """
+
+        buf = ''
+        while True:
+            c = self.raw_text[self.index]
+            self.index += 1
+            if self.debug:
+                print_cyan(c)
+
+            if c == '\\':
+                # We are on a backslash and there is another character after the backslash
+                # to parse. Handle this case specially since gdb escaped it for us
+
+                # Get the next char that is being escaped
+                c2 = self.raw_text[self.index]
+                self.index += 1
+                if c2 in chars_to_remove_gdb_escape:
+                    # only store the escaped character in the buffer; don't store the backslash
+                    # (don't leave it escaped)
+                    buf += c2
+                else:
+                    # store the backslash and the following character in the buffer (leave it escaped)
+                    buf += c + c2
+
+            elif c == '"':
+                # Quote is closed. Exit (and don't include the end quote).
+                break
+
+            else:
+                # capture this character, and keep capturing
+                buf += c
+        return buf
--- pygdbmi/tests/test_app.py
+++ pygdbmi/tests/test_app.py
@@ -10,6 +10,8 @@ import os
 import random
 import unittest
 import subprocess
+from time import time
+from pygdbmi.StringStream import StringStream
 from pygdbmi.gdbmiparser import parse_response, assert_match
 from pygdbmi.gdbcontroller import GdbController, NoGdbProcessError, GdbTimeoutError
 
@@ -33,6 +35,7 @@ class TestPyGdbMi(unittest.TestCase):
         assert_match(parse_response('@"\b\f\n\r\t\""'), {"type": "target", "payload": '\b\f\n\r\t\"', 'message': None})
         assert_match(parse_response('&""'), {"type": "log", "payload": "", 'message': None})
         assert_match(parse_response('&"\b\f\n\r\t\""'), {"type": "log", "payload": '\b\f\n\r\t\"', 'message': None})
+        assert_match(parse_response('&"\\"'), {"type": "log", "payload": '\\', 'message': None})  # test that an escaped backslash gets captured
 
         # Test that a dictionary with repeated keys (a gdb bug) is gracefully worked-around  by pygdbmi
         # See https://sourceware.org/bugzilla/show_bug.cgi?id=22217
@@ -185,11 +188,78 @@ class TestPyGdbMi(unittest.TestCase):
                 assert(gdbmi._incomplete_output[stream] is None)
 
 
+class TestPerformance(unittest.TestCase):
+
+    def get_test_input(self, n_repetitions):
+        data = ', '.join(['"/a/path/to/parse/' + str(i) + '"' for i in range(n_repetitions)])
+        return '=test-message,test-data=[' + data + ']'
+
+    def get_avg_time_to_parse(self, input_str, num_runs):
+        avg_time = 0
+        for _ in range(num_runs):
+            t0 = time()
+            parse_response(input_str)
+            t1 = time()
+            time_to_run = t1 - t0
+            avg_time += time_to_run / num_runs
+        return avg_time
+
+    def test_big_o(self):
+        num_runs = 2
+
+        large_input_len = 100000
+
+        single_input = self.get_test_input(1)
+        large_input = self.get_test_input(large_input_len)
+
+        t_small = self.get_avg_time_to_parse(single_input, num_runs)
+        t_large = self.get_avg_time_to_parse(large_input, num_runs)
+        bigo_n = ((t_large / large_input_len) / t_small)
+        assert(bigo_n < 1)  # with old parser, this was over 3
+
+
+class TestStringStream(unittest.TestCase):
+
+    def test_api(self):
+        raw_text = 'abc- "d" "\"ef\"" g'
+        stream = StringStream(raw_text)
+        assert(stream.index == 0)
+        assert(stream.len == len(raw_text))
+
+        buf = stream.read(1)
+        assert_match(buf, 'a')
+        assert(stream.index == 1)
+
+        stream.seek(-1)
+        assert(stream.index == 0)
+
+        buf = stream.advance_past_chars(['"'])
+        # assert_match(buf, 'abc- ')
+        # assert_match(raw_text[stream.index - 1], '-')
+        # assert_match(raw_text[stream.index], ' ')
+        # buf = stream.advance_past_chars(['"'])
+        # print(buf)
+        buf = stream.advance_past_string_with_gdb_escapes()
+        assert_match(buf, 'd')
+
+        buf = stream.advance_past_chars(['"'])
+        buf = stream.advance_past_chars(['"'])
+        buf = stream.advance_past_string_with_gdb_escapes()
+        assert_match(buf, 'ef')
+
+        # read way past end to test it gracefully returns the
+        # remainder of the string without failing
+        buf = stream.read(50)
+        assert_match(buf, '" g')
+
+
 def main():
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
 
+    suite.addTests(loader.loadTestsFromTestCase(TestStringStream))
     suite.addTests(loader.loadTestsFromTestCase(TestPyGdbMi))
+    suite.addTests(loader.loadTestsFromTestCase(TestPerformance))
 
     runner = unittest.TextTestRunner(verbosity=1)
     result = runner.run(suite)
