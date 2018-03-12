39c1a1798994bcb2168a6c0995b32c63786b8572
mariusmue <marius.muench@eurecom.fr>
2018-03-12 18:20:54 +0100

    Wait for gdb to actually terminate upon exit() (#31)

--- pygdbmi/gdbcontroller.py
+++ pygdbmi/gdbcontroller.py
@@ -359,6 +359,7 @@ class GdbController():
         Returns: None"""
         if self.gdb_process:
             self.gdb_process.terminate()
+            self.gdb_process.communicate()
         self.gdb_process = None
         return None
 
