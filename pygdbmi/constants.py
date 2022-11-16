905930c153e8a3602fd83b891f02fa7b7eb91c1a
BAROLLET Theo <theo.barollet@inria.fr>
2022-11-16 16:01:25 +0100

    first draft of thread based implementation to make the code public

--- pygdbmi/constants.py
+++ pygdbmi/constants.py
@@ -2,19 +2,7 @@ import os
 
 
 __all__ = [
-    "DEFAULT_GDB_TIMEOUT_SEC",
-    "DEFAULT_TIME_TO_CHECK_FOR_ADDITIONAL_OUTPUT_SEC",
-    "GdbTimeoutError",
     "USING_WINDOWS",
 ]
 
-
-DEFAULT_GDB_TIMEOUT_SEC = 1
-DEFAULT_TIME_TO_CHECK_FOR_ADDITIONAL_OUTPUT_SEC = 0.2
 USING_WINDOWS = os.name == "nt"
-
-
-class GdbTimeoutError(ValueError):
-    """Raised when no response is recieved from gdb after the timeout has been triggered"""
-
-    pass
